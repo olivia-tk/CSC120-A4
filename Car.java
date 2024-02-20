96f7d0f1a2f7b4d6a4eda480b1ac6e01474ceb53
Olivia Kraft <okraft@smith.edu>
2024-02-20 08:54:00 -0500

    First attempt

--- Car.java
+++ Car.java
@@ -1,3 +1,55 @@
+import java.util.ArrayList;
+
 public class Car {
+    private ArrayList<Passenger> passengers;
+    private int maxCapacity;
+
+    // Constructor
+    public Car(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+        this.passengers = new ArrayList<>();
+        }
+    // Accessors
+    public int getCapacity() {
+        return maxCapacity;
+    }
+
+    public int seatsRemaining() {
+        return maxCapacity - passengers.size();
+    }
+
+    // Add passenger to the car
+    public boolean addPassenger(Passenger passenger) {
+        if (seatsRemaining() > 0) {
+            passengers.add(passenger);
+            return true;
+        } else {
+            System.out.println("No available seats in the car.");
+            return false;
+        }
+    }
+
+    // Remove passenger from the car
+    public boolean removePassenger(Passenger passenger) {
+        if (passengers.contains(passenger)) {
+            passengers.remove(passenger);
+            return true;
+        } else {
+            System.out.println("Passenger is not onboard the car.");
+            return false;
+        }
+    }
+
+    // Print the list of passengers in the car
+    public void printManifest() {
+        if (!passengers.isEmpty()) {
+            System.out.println("Passengers onboard:");
+            for (Passenger passenger : passengers) {
+                System.out.println("- " + passenger.getName());
+            }
+        } else {
+            System.out.println("This car is EMPTY.");
+        }
+    }
 
 }
--- Engine.java
+++ Engine.java
@@ -1,3 +1,47 @@
 public class Engine {
+    private FuelType fuelType;
+    private double currentFuelLevel;
+    private double maxFuelLevel;
 
-}
+    // Constructor
+    public Engine(FuelType fuelType, double maxFuelLevel) {
+        this.fuelType = fuelType;
+        this.maxFuelLevel = maxFuelLevel;
+        this.currentFuelLevel = maxFuelLevel;
+    }
+    public FuelType getFuelType() {
+        return fuelType;
+    }
+
+    public double getCurrentFuelLevel() {
+        return currentFuelLevel;
+    }
+
+    public double getMaxFuelLevel() {
+        return maxFuelLevel;
+    }
+
+    // Refuel method
+    public void refuel() {
+        currentFuelLevel = maxFuelLevel;
+    }
+
+    // Go method
+    public boolean go() {
+        if (currentFuelLevel > 0) {
+            System.out.println("Remaining fuel level: " + currentFuelLevel);
+            currentFuelLevel -= 10;
+            return true;
+        } else {
+            System.out.println("Out of fuel.");
+            return false;
+        }
+    }
+
+    // Sample usage in the main method
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
+        while (myEngine.go()) {
+        }
+    }
+    }
